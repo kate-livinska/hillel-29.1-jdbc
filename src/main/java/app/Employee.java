e96107e673b50978be6f1334c1291e874fa97a31
Kateryna Livinska <kateryna.livinska@gmail.com>
2025-01-17 12:07:12 +0100

    Add Employee class

---
+++ src/main/java/app/Employee.java
@@ -0,0 +1,71 @@
+package app;
+
+public class Employee {
+    int id;
+    String name;
+    int age;
+    String position;
+    Float salary;
+
+    public Employee() {
+        this("", 0, "", 0.0F);
+    }
+
+    public Employee(String name, int age, String position, Float salary) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", position='" + position + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
